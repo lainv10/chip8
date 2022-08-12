c03a852020974379bef84183607d16861c5f7998
jpglain <111026289+jpglain@users.noreply.github.com>
2022-08-11 17:03:22 -0700

    Setup logger

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -1,9 +1,18 @@
+// hide the console window on non-debug builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod app;
 mod audio;
 mod gui;
 mod renderer;
 
 fn main() {
+    setup_logger();
+    run_native();
+}
+
+/// Initialize and run a native [`eframe`] app.
+fn run_native() {
     eframe::run_native(
         "chip8!",
         eframe::NativeOptions {
@@ -12,4 +21,27 @@ fn main() {
         },
         Box::new(|cc| Box::new(app::App::new(cc))),
     );
+}
+
+/// Setup the [`fern`] logger.
+fn setup_logger() {
+    #[cfg(debug_assertions)]
+    let level = log::LevelFilter::Debug;
+
+    #[cfg(not(debug_assertions))]
+    let level = log::LevelFilter::Info;
+
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}] {}",
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(level)
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
 }
